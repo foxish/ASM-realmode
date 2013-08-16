d471345e98b2fdf387bba60667ecb445f137a361
DarkCthulhu <anirudh4444@gmail.com>
2013-08-17 01:16:00 +0530

    added more addressing methods

--- BIOS-Addressing.asm
+++ BIOS-Addressing.asm
@@ -2,9 +2,23 @@
 ;org 7c00h ; one way to indicate that all offsets should be calculated from this address
 
 mov ah, 0x0e ;for printing characters
+
+; adding offset manually 
 mov bx, character ; load the relative address
 add bx, 7c00h ; add offset
-mov al, [bx] ;load item at address specified
+mov al, [bx] ; load item at address specified
+int 0x10
+
+;load item at address using segment registers
+mov bx, 0x07c0
+mov ds, bx
+mov al, [character]
+int 0x10
+
+;load item at address using explicit segment offset register
+mov bx, 0x07c0
+mov es, bx
+mov al, [es:character]
 int 0x10
 
 jmp $ ; infinite loop
